720f580ec3ad8a3395039320f9f95b80d7753ff3
Adam <lucia.adam@gmail.com>
2017-03-20 00:07:30 -0400

    add Provider, Router, and Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/items" component={ItemsPage}>
+          <Route path="/items/new" component={ItemsNew} />
+          <Route path="/items/:id" component={ItemsShow}
+        </Route>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
