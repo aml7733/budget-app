229a6594723dce86ea707607354b256a4668cacb
Adam <lucia.adam@gmail.com>
2017-03-20 21:39:33 -0400

    trying semantic ui again

--- client/src/reducers/itemsReducer.js
+++ client/src/reducers/itemsReducer.js
@@ -2,6 +2,9 @@ const itemsReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_ITEM':
       return state.concat(action.payload);
+    case 'DELETE_ITEM':
+      return state.filter
+      break;
     default:
       return state;
   }
