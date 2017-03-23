80bbeb82f4244bd2e214cf3b9dd6f3358f470cdb
Adam <lucia.adam@gmail.com>
2017-03-22 22:12:59 -0400

    add sort items button to items component

--- client/src/containers/Items.js
+++ client/src/containers/Items.js
@@ -7,6 +7,14 @@ import Liabilities from '../components/Liabilities'
 import Total from '../components/Total'
 
 class Items extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      assets: [],
+      liabilities: []
+    }
+  }
 
   componentWillMount(){
     if (this.props.items.length === 0) {
@@ -14,33 +22,46 @@ class Items extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      assets: nextProps.items[0].assets,
+      liabilities: nextProps.items[0].liabilities
+    })
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const sortedAssets = this.state.assets.sort((a, b) => {return parseFloat(a.amount) - parseFloat(b.amount)})
+    const sortedLiabilities = this.state.liabilities.sort((a, b) => {return parseFloat(a.amount) - parseFloat(b.amount)})
+
+    this.setState({
+      assets: sortedAssets,
+      liabilities: sortedLiabilities
+    })
+  }
+
   render() {
-    let assets, liabilities;
-    if (this.props.items[0]) {
-      assets = this.props.items[0].assets;
-      liabilities = this.props.items[0].liabilities;
-    }
+    const itemList = this.props.items[0]
 
     return (
       <div>
-          <Assets items={assets} /><br/>
-          <Liabilities items={liabilities} /><br/>
-          <Total items={this.props.items[0]} /><br/>
+          <Assets items={!!itemList ? this.state.assets : null} /><br/>
+          <Liabilities items={!!itemList ? this.state.liabilities : null} /><br/>
+          <Total items={itemList} /><br/>
           {this.props.children}<br/>
+          <button className="ui primary basic button" onClick={(event) => this.handleClick(event)}>Sort Items</button>
       </div>
     );
   }
 };
 
 const mapStateToProps = (state) => {
-  console.log('in map state to props Items')
   return {
     items: state.items,
   };
 }
 
 const mapDispatchToProps = (dispatch) => {
-  console.log('in map dispatch to props Items')
   return bindActionCreators({
       fetchItems: fetchItems
     }, dispatch);
